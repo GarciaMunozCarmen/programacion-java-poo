c2943d6f1237cb56b7de9130f8a46494b72cd9d1
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-12-05 14:16:40 +0100

    publicacion-finished, PerfilRedSocial-TODO

---
+++ src/main/java/com/carmengarciamunoz/poo/redsocial/PerfilRedSocial.java
@@ -0,0 +1,17 @@
+package com.carmengarciamunoz.poo.redsocial;
+
+public class PerfilRedSocial {
+	
+//	ATRIBUTOS
+	private String userName;
+	private String publicName;
+	private String biography;
+	private String city;
+	private int followers;
+	private int posts;
+//	private String status;
+	private boolean verified;
+
+//	CONSTRUCTOR
+	
+}
---
+++ src/main/java/com/carmengarciamunoz/poo/redsocial/Publicacion.java
@@ -0,0 +1,53 @@
+package com.carmengarciamunoz.poo.redsocial;
+
+import java.time.LocalTime;
+
+public class Publicacion {
+	
+//	ATRIBUTOS
+	private String text;
+	private LocalTime dateTime;
+	private int likes;
+	
+//	CONSTRUCTORES
+	public Publicacion (String content) {
+		this.text = content;
+		this.dateTime = LocalTime.now();
+		this.likes = 0;
+	}
+
+//	GETTERS Y SETTERS
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalTime getdateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	
+//	MÉTODOS
+	public void likePost() {
+		likes++;
+	}
+	
+	public void show() {
+		System.out.printf("La publicación '%s' publicada %s tiene %d likes", text, dateTime, likes);
+	}
+	
+}
