d810c37812ca12ff0aaca1838de8165edc74fad3
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-12-09 09:26:07 +0100

    update PerfilRedSocial

--- src/main/java/com/carmengarciamunoz/poo/redsocial/PerfilRedSocial.java
+++ src/main/java/com/carmengarciamunoz/poo/redsocial/PerfilRedSocial.java
@@ -13,5 +13,64 @@ public class PerfilRedSocial {
 //	private boolean verified;
 
 //	CONSTRUCTOR
+	public PerfilRedSocial (String userName, String publicName, String biography, String city) {
+		this.userName = userName;
+		this.publicName = publicName;
+		this.biography = biography;
+		this.city = city;
+		this.posts = 0;
+	}
+	
+// SETTERS Y GETTERS
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPublicName() {
+		return publicName;
+	}
+
+	public void setPublicName(String publicName) {
+		this.publicName = publicName;
+	}
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getFollowers() {
+		return followers;
+	}
+
+	public void setFollowers(int followers) {
+		this.followers = followers;
+	}
+
+	public int getPosts() {
+		return posts;
+	}
+
+	public void setPosts(int posts) {
+		this.posts = posts;
+	}
+	
+//	MÉTODOS
 	
 }
+
